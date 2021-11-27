6ea880fc34fbfa92944e3c0254aed65f6c5f98fa
depth81 <petv1981@gmail.com>
2021-11-27 15:06:08 -0500

    correction

--- apimongoreact/src/App.js
+++ apimongoreact/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+toast.configure();
 class App extends React.Component{
   
   constructor(props){
