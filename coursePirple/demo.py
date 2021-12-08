f2bac2f4fb6fa674dab7f188924dd04eb397fa19
depth81 <petv1981@gmail.com>
2021-12-07 21:26:34 -0500

    update

--- coursePirple/demo.py
+++ coursePirple/demo.py
@@ -1,24 +1,59 @@
-from logging import error
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for, g
+import model
 
 app = Flask(__name__)
+app.secret_key = 'jumpjacks'
+
+username = ''
+user = model.check_users()
 
 @app.route('/', methods=['GET','POST'])
 def home():
-    if request.method=='GET':
-        return render_template('index.html',message='Aloha Aloha')
+    if 'username' in session:
+        g.user=session['username']
+        return render_template('loggedin.html',message='<img src=static/img/coffee.jpg>')
+    return render_template('homepage.html',message='Login to the page or sign up')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        session.pop('username',None)
+        areyouuser = request.form['username']
+        pwd=model.check_pw(areyouuser)
+        if request.form['password'] == pwd:
+            session['username'] = request.form['username']
+            return redirect(url_for('home')) #Refers to function home()
+    return render_template('index.html')
+
+@app.before_request
+def before_request():
+    g.username = None
+    if 'username' in session:
+        g.username = session['username']
+
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+    if request.method =='GET':
+        message='Please sign up first'
+        return render_template('signup.html', message=message)
     else:
         username = request.form['username']
         password = request.form['password']
-        if username=='paulo' and password=='123':
-            return render_template('football.html', message='Login successful')
-        else:
-            error_message = 'Hint: The usual three numbers for paulo!'
-            return render_template('index.html', message=error_message)
+        favorite_color = request.form['favorite_color']
+        message=model.signup(username,password,favorite_color)
+        return render_template('signup.html', message=message)
 
-@app.route('/football', methods=['GET'])
-def football():
-    return render_template('football.html')
+@app.route('/getsession')
+def getsession():
+    if 'username' in session:
+        return session['username']
+    return(url_for('login')) #refers to function login()
+
+@app.route('/logout')
+def logout():
+    session.pop('username',None)
+    return redirect(url_for('home')) #it refers to the function home()
+
 
 if __name__ == '__main__':
     app.run(port=7000, debug=True)
